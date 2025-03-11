ad2c8ad7477a1ba65609762b18ccdcdc76911cda
Tsurubara-UTcode <168950066+Tsurubara-UTcode@users.noreply.github.com>
2025-03-11 11:35:00 +0900

    微調整

--- src/App.jsx
+++ src/App.jsx
@@ -235,7 +235,7 @@ export default function QuizGame() {
       <p className="header custom-font2" style={{marginLeft:"20pt", marginTop: '40pt', color: 'white', fontSize: '4rem' ,...borderStyle}}>
         のお
       </p>
-      <p className="header custom-font2" style={{ marginLeft: '240pt', marginTop: '40pt', color: 'white', fontSize: '6rem' }}>
+      <p className="header custom-font2" style={{ marginLeft: '260pt', marginTop: '40pt', color: 'white', fontSize: '6rem' }}>
         えとらんじえ
       </p>
       <p className="header custom-font" style={{ marginLeft: '60pt', marginTop: '40pt', color: 'white', fontWeight: 'bold', fontSize: '1.5rem' }}>
