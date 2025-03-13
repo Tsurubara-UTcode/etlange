898163d227b064a18f8abcd229cbaf4425d9115b
Tsurubara-UTcode <168950066+Tsurubara-UTcode@users.noreply.github.com>
2025-03-13 10:35:01 +0900

    序盤スライド削除

--- src/App.jsx
+++ src/App.jsx
@@ -209,7 +209,7 @@ export default function QuizGame() {
           }}
         >
           <video ref={videoRef} width="100%" height="100%" controls>
-            <source src="/materials/video.mp4" type="video/mp4" />
+            <source src="/materials/slides/creditmovie_light.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
